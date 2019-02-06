b17aa7469bc27e2a1345f28472e6e1423f0e3a5f
aaron <xakong@outlook.com>
2019-02-06 04:48:09 -0500

    finishing components, finding the selected movie render bug

---
+++ src/components/elements/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+   return (
+      <div>
+         <h1>This page does not exist! Please try another!</h1>
+      </div>
+   )
+}
+
+export default NotFound;
